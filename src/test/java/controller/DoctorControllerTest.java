6118231383bcf53d7a2b15e9c5b45e341c4612e0
Aris <arismiuta01@gmail.com>
2018-03-13 19:35:30 +0200

    Add whack project.

---
+++ src/test/java/controller/DoctorControllerTest.java
@@ -0,0 +1,14 @@
+package controller;
+
+import org.junit.Assert.*;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DoctorControllerTest {
+    @Test
+    public void addPatient() throws Exception {
+        assertEquals(1, 2);
+    }
+
+}
